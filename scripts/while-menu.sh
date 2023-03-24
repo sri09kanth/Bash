cdab4c5f3f83e6d76e85044e55526f83e44b7c31
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-24 10:28:12 +0530

    Update while-menu.sh

--- scripts/while-menu.sh
+++ scripts/while-menu.sh
@@ -10,7 +10,7 @@ while true; do
         3. Display Home Space Utilizatio
         0. Quit
     read -p "Enter selection [0-3] >
-    case "$REPLY" in
+    case "$REPLY" i
         0
             break
             ;;

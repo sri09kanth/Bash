dc2e0400ebef0c2f5524736f0470b042cd64ebf1
Srikanth Mergu <105923827+sri09kanth@users.noreply.github.com>
2023-03-24 10:27:25 +0530

    Update while-menu.sh

--- scripts/while-menu.sh
+++ scripts/while-menu.sh
@@ -9,7 +9,6 @@ while true; do
         2. Display Disk Space
         3. Display Home Space Utilization
         0. Quit
-EOF
     read -p "Enter selection [0-3] > 
     case "$REPLY" in
         0)
